25c74f750c5a00cdc947dabd3c66c393957b9ffb
alishancode <alishan@byldd.com>
2024-05-27 22:24:50 +0530

    router setup

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from "vue-router";
+import ContactListView from "../views/ContactListView.vue";
+import EditContactView from "../views/EditContactView.vue";
+import ViewContactView from "../views/ViewContactView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: ContactListView,
+    },
+    {
+      path: "/edit/:id",
+      name: "Edit",
+      component: EditContactView,
+    },
+    {
+      path: "/contact/:id",
+      name: "View",
+      component: ViewContactView,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: () => import("../views/NotFoundView.vue"),
+    },
+  ],
+});
+
+export default router;
